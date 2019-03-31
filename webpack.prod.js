1b7d2ce6c74f398bd900f1c161b74d459d9b8104
HuyTrinh <dinhhuyams@gmail.com>
2019-03-31 11:19:37 +0300

    feat: add webpack files

---
+++ webpack.prod.js
@@ -0,0 +1,17 @@
+const merge = require('webpack-merge');
+const common = require('./webpack.common');
+
+module.exports = merge(common, {
+  mode: 'production',
+
+  output: {
+    filename: '[name].[chunkhash].js',
+    chunkFilename: '[name].[chunkhash].chunk.js',
+    publicPath: '/',
+  },
+
+  module: {
+    rules: [
+    ]
+  }
+})
