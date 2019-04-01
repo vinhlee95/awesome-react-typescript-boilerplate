31c3c4ed0da4616fa6e016446a4c6dd320e6e700
Vinh Le <vinh@vertics.co>
2019-04-01 22:38:53 +0300

    fix:tab sizing issue

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -3,80 +3,80 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
-  entry: path.resolve(__dirname, '../src/index.tsx'),
+	entry: path.resolve(__dirname, '../src/index.tsx'),
 
-  output: {
-    path: path.resolve(__dirname,  '../dist'),
-    publicPath: "/"
-		},
+	output: {
+		path: path.resolve(__dirname,  '../dist'),
+		publicPath: "/"
+	},
 
-		resolve: {
-			extensions: ['.ts', '.tsx', '.js']
-		},
+	resolve: {
+		extensions: ['.ts', '.tsx', '.js']
+	},
 
-  module: {
-    rules: [
-						{
-							test: /\.tsx?$/,
-							loader: 'awesome-typescript-loader'
+	module: {
+		rules: [
+			{
+				test: /\.tsx?$/,
+				loader: 'awesome-typescript-loader'
+			},
+			{
+				test: /\.(eot|otf|ttf|woff|woff2)$/,
+				use: 'file-loader'
+			},
+			{
+				test: /\.svg$/,
+				use: [
+					{
+						loader: 'svg-url-loader',
+						options: {
+							// Inline files smaller than 10 kB
+							limit: 10 * 1024,
+							noquotes: true
 						},
-      {
-        test: /\.(eot|otf|ttf|woff|woff2)$/,
-        use: 'file-loader'
-      },
-      {
-        test: /\.svg$/,
-        use: [
-          {
-            loader: 'svg-url-loader',
-            options: {
-              // Inline files smaller than 10 kB
-              limit: 10 * 1024,
-              noquotes: true
-            },
-          },
-        ],
-      },
-      {
-        test: /\.(jpg|png|gif)$/,
-        use: [
-          {
-            loader: 'url-loader',
-            options: {
-              // Inline files smaller than 10 kB
-              limit: 10 * 1024
-            },
-          },
-          {
-            loader: 'image-webpack-loader',
-            options: {
-              mozjpeg: {
-                enabled: false
-                // NOTE: mozjpeg is disabled as it causes errors in some Linux environments
-                // Try enabling it in your environment by switching the config to:
-                // enabled: true,
-                // progressive: true,
-              },
-              gifsicle: {
-                interlaced: false
-              },
-              optipng: {
-                optimizationLevel: 7
-              },
-              pngquant: {
-                quality: '65-90',
-                speed: 4
-              }
-            }
-          }
-        ]
-      }
-    ]
-  },
+					},
+				],
+			},
+			{
+				test: /\.(jpg|png|gif)$/,
+				use: [
+					{
+						loader: 'url-loader',
+						options: {
+							// Inline files smaller than 10 kB
+							limit: 10 * 1024
+						},
+					},
+					{
+						loader: 'image-webpack-loader',
+						options: {
+							mozjpeg: {
+								enabled: false
+								// NOTE: mozjpeg is disabled as it causes errors in some Linux environments
+								// Try enabling it in your environment by switching the config to:
+								// enabled: true,
+								// progressive: true,
+							},
+							gifsicle: {
+								interlaced: false
+							},
+							optipng: {
+								optimizationLevel: 7
+							},
+							pngquant: {
+								quality: '65-90',
+								speed: 4
+							}
+						}
+					}
+				]
+			}
+		]
+	},
 
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.resolve(__dirname, '../public/index.html')
-    })
-  ]
+	plugins: [
+		new HtmlWebpackPlugin({
+			template: path.resolve(__dirname, '../public/index.html')
+		})
+	]
 }
