219890debc8b18e7cead4ae2a878b42168322b8a
HuyTrinh <huy@makehelsinki.com>
2019-04-01 18:18:25 +0300

    feat: add mini-css-extract-plugin for prod webpack

--- webpack/webpack.prod.js
+++ webpack/webpack.prod.js
@@ -1,5 +1,6 @@
 const merge = require('webpack-merge');
 const common = require('./webpack.common');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 
 module.exports = merge(common, {
   mode: 'production',
@@ -12,6 +13,27 @@ module.exports = merge(common, {
 
   module: {
     rules: [
+      {
+        test: /\.(?:sa|sc|c)ss$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              modules: true,
+              localIdentName: '[path][name]__[local]--[hash:base64:5]',
+            },
+          },
+          'sass-loader'
+        ],
+      }
     ]
-  }
+  },
+
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: 'style.[contenthash].css'
+    })
+  ]
 })
