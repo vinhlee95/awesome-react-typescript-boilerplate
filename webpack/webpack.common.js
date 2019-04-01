5e2c94be59f234a8612c79d4f433969ee49678da
HuyTrinh <huy@makehelsinki.com>
2019-04-01 10:33:53 +0300

    feat: add html-webpack-plugin

--- webpack/webpack.common.js
+++ webpack/webpack.common.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
 
@@ -66,5 +67,8 @@ module.exports = {
   },
 
   plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html')
+    })
   ]
 }
