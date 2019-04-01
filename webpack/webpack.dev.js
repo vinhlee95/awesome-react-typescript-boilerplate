e52de79edf45d6429961e3a751bd9d177e30bc15
HuyTrinh <huy@makehelsinki.com>
2019-04-01 10:52:13 +0300

    feat: add devtool to dev webpack

--- webpack/webpack.dev.js
+++ webpack/webpack.dev.js
@@ -5,6 +5,8 @@ const common = require('./webpack.common');
 module.exports = merge(common, {
   mode: 'development',
 
+  devtool: 'source-map',
+
   devServer: {
     contentBase: path.resolve(__dirname, '../public/index.html'),
     port: 8000,
