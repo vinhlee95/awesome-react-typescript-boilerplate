3a401165858f5e55b7faf9344af7e2ac0d8b0a6b
HuyTrinh <huy@makehelsinki.com>
2019-04-08 09:09:38 +0300

    feat: add ErrorBoundaries to handle unexpected error, disable strict mode and noImplicitAny in tsconfig

--- src/index.tsx
+++ src/index.tsx
@@ -3,5 +3,12 @@ import * as ReactDOM from 'react-dom'
 import './index.scss'
 
 import App from './App'
+import ErrorBoundaries from './shared/components/ErrorBoundary'
 
-ReactDOM.render(<App />, document.getElementById('root'))
+const app = (
+	<ErrorBoundaries>
+		<App />
+	</ErrorBoundaries>
+)
+
+ReactDOM.render(app, document.getElementById('root'))
