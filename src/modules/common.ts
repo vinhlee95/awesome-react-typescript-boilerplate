cddca2122ed8d407ba521a450315362e4c890678
Vinh Le <vinh@vertics.co>
2019-04-12 19:22:14 +0300

    feat: add immer and get used of it in reducer commons

--- src/modules/common.ts
+++ src/modules/common.ts
@@ -1,35 +1,39 @@
 import BaseModel from '../models/baseModel'
+import PostModel from '../models/post'
+import PostsModel from '../models/posts'
+
+import produce from 'immer'
 
 export const startLoading = (state: BaseModel) => {
-	return {
-		...state,
-		loading: true,
-		error: undefined,
-	}
+	return produce(state, draft => {
+		draft.loading = true
+		draft.error = undefined
+	})
 }
 
 export const endLoading = (state: BaseModel, error) => {
-	return {
-		...state,
-		loading: false,
-		error,
-	}
+	return produce(state, draft => {
+		draft.loading = false
+		draft.error = error
+	})
 }
 
-export const updateData = (state: BaseModel, data) => {
-	return {
-		...state,
-		loading: false,
-		error: undefined,
-		...data,
-	}
+export const updateData = (state: PostModel, data) => {
+	const { id, userId, title, body } = data
+	return produce(state, draft => {
+		draft.loading = false
+		draft.error = undefined
+		draft.id = id
+		draft.userid = userId
+		draft.title = title
+		draft.body = body
+	})
 }
 
-export const updateListData = (state: BaseModel, data) => {
-	return {
-		...state,
-		loading: false,
-		error: undefined,
-		list: data,
-	}
+export const updateListData = (state: PostsModel, data) => {
+	return produce(state, draft => {
+		draft.loading = false
+		draft.error = undefined
+		draft.list = data
+	})
 }
