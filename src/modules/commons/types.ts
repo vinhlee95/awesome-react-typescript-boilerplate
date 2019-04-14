e3ca5c2b7fc43832795f349fdac32b8009d4e548
Vinh Le <vinh@vertics.co>
2019-04-14 22:05:35 +0300

    refactor: change prefix of types to be lowercase for better visibility

--- src/modules/commons/types.ts
+++ src/modules/commons/types.ts
@@ -1,29 +1,25 @@
 // Common types
 
 export const get = {
-	start: (modelName: string) => `${modelName.toUpperCase()}_GET_MODEL`,
-	success: (modelName: string) =>
-		`${modelName.toUpperCase()}_GET_MODEL_SUCCESS`,
-	fail: (modelName: string) => `${modelName.toUpperCase()}_GET_MODEL_FAIL`,
+	start: (modelName: string) => `${modelName}_GET_MODEL`,
+	success: (modelName: string) => `${modelName}_GET_MODEL_SUCCESS`,
+	fail: (modelName: string) => `${modelName}_GET_MODEL_FAIL`,
 }
 
 export const post = {
-	start: (modelName: string) => `${modelName.toUpperCase()}_POST_MODEL`,
-	success: (modelName: string) =>
-		`${modelName.toUpperCase()}_POST_MODEL_SUCCESS`,
-	fail: (modelName: string) => `${modelName.toUpperCase()}_POST_MODEL_FAIL`,
+	start: (modelName: string) => `${modelName}_POST_MODEL`,
+	success: (modelName: string) => `${modelName}_POST_MODEL_SUCCESS`,
+	fail: (modelName: string) => `${modelName}_POST_MODEL_FAIL`,
 }
 
 export const put = {
-	start: (modelName: string) => `${modelName.toUpperCase()}_PUT_MODEL`,
-	success: (modelName: string) =>
-		`${modelName.toUpperCase()}_PUT_MODEL_SUCCESS`,
-	fail: (modelName: string) => `${modelName.toUpperCase()}_PUT_MODEL_FAIL`,
+	start: (modelName: string) => `${modelName}_PUT_MODEL`,
+	success: (modelName: string) => `${modelName}_PUT_MODEL_SUCCESS`,
+	fail: (modelName: string) => `${modelName}_PUT_MODEL_FAIL`,
 }
 
 export const remove = {
-	start: (modelName: string) => `${modelName.toUpperCase()}_DELETE_MODEL`,
-	success: (modelName: string) =>
-		`${modelName.toUpperCase()}_DELETE_MODEL_SUCCESS`,
-	fail: (modelName: string) => `${modelName.toUpperCase()}_DELETE_MODEL_FAIL`,
+	start: (modelName: string) => `${modelName}_DELETE_MODEL`,
+	success: (modelName: string) => `${modelName}_DELETE_MODEL_SUCCESS`,
+	fail: (modelName: string) => `${modelName}_DELETE_MODEL_FAIL`,
 }
