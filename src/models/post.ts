ef329726a963d8ac0d1bcfb8de585279dc852cc1
Vinh Le <vinh@vertics.co>
2019-04-15 09:22:21 +0300

    refactor: remove optional type in Post model props

--- src/models/post.ts
+++ src/models/post.ts
@@ -1,8 +1,8 @@
 import BaseModel from './bases/baseModel'
 
 export default interface Post extends BaseModel {
-	id?: number
-	userid?: number
-	title?: string
-	body?: string
+	id: number
+	userid: number
+	title: string
+	body: string
 }
