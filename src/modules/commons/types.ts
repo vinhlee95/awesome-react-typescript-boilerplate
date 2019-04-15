5af115115ad1fb15ff6f780f8285649c97253317
Vinh Le <vinh@vertics.co>
2019-04-15 09:14:20 +0300

    refactor: change modelName in types to lowercase

--- src/modules/commons/types.ts
+++ src/modules/commons/types.ts
@@ -1,25 +1,25 @@
 // Common types
 
 export const get = {
-	start: (modelName: string) => `${modelName}_GET_MODEL`,
-	success: (modelName: string) => `${modelName}_GET_MODEL_SUCCESS`,
-	fail: (modelName: string) => `${modelName}_GET_MODEL_FAIL`,
+	start: (modelName: string) => `${modelName}.GET_MODEL`,
+	success: (modelName: string) => `${modelName}.GET_MODEL_SUCCESS`,
+	fail: (modelName: string) => `${modelName}.GET_MODEL_FAIL`,
 }
 
 export const post = {
-	start: (modelName: string) => `${modelName}_POST_MODEL`,
-	success: (modelName: string) => `${modelName}_POST_MODEL_SUCCESS`,
-	fail: (modelName: string) => `${modelName}_POST_MODEL_FAIL`,
+	start: (modelName: string) => `${modelName}.POST_MODEL`,
+	success: (modelName: string) => `${modelName}.POST_MODEL_SUCCESS`,
+	fail: (modelName: string) => `${modelName}.POST_MODEL_FAIL`,
 }
 
 export const put = {
-	start: (modelName: string) => `${modelName}_PUT_MODEL`,
-	success: (modelName: string) => `${modelName}_PUT_MODEL_SUCCESS`,
-	fail: (modelName: string) => `${modelName}_PUT_MODEL_FAIL`,
+	start: (modelName: string) => `${modelName}.PUT_MODEL`,
+	success: (modelName: string) => `${modelName}.PUT_MODEL_SUCCESS`,
+	fail: (modelName: string) => `${modelName}.PUT_MODEL_FAIL`,
 }
 
 export const remove = {
-	start: (modelName: string) => `${modelName}_DELETE_MODEL`,
-	success: (modelName: string) => `${modelName}_DELETE_MODEL_SUCCESS`,
-	fail: (modelName: string) => `${modelName}_DELETE_MODEL_FAIL`,
+	start: (modelName: string) => `${modelName}.DELETE_MODEL`,
+	success: (modelName: string) => `${modelName}.DELETE_MODEL_SUCCESS`,
+	fail: (modelName: string) => `${modelName}.DELETE_MODEL_FAIL`,
 }
