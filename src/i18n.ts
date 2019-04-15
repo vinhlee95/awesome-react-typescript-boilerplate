9e190a033aed524b562fc4f07fdf12da7a4e9ff0
HuyTrinh <huy@makehelsinki.com>
2019-04-15 14:30:14 +0300

    refactor: remove debug in i18n

--- src/i18n.ts
+++ src/i18n.ts
@@ -16,7 +16,6 @@ i18n
 	// for all options read: https://www.i18next.com/overview/configuration-options
 	.init({
 		fallbackLng: 'en',
-		debug: true,
 
 		interpolation: {
 			escapeValue: false, // not needed for react as it escapes by default
