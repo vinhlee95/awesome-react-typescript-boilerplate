4d4f964623f1925f05a18db969b1ae638f3203f4
HuyTrinh <huy@makehelsinki.com>
2019-04-16 18:35:50 +0300

    feat: add create update delete model to moduleActions

--- src/modules/commons/moduleActions.ts
+++ src/modules/commons/moduleActions.ts
@@ -11,23 +11,35 @@ const useModuleActions = (moduleName: string, path: string) => {
 		GET_MODEL_SUCCESS: `${moduleName}.GET_MODEL_SUCCESS`,
 		GET_MODEL_FAIL: `${moduleName}.GET_MODEL_FAIL`,
 
+		CREATE_MODEL: `${moduleName}.CREATE_MODEL`,
+		CREATE_MODEL_SUCCESS: `${moduleName}.CREATE_MODEL_SUCCESS`,
+		CREATE_MODEL_FAIL: `${moduleName}.CREATE_MODEL_FAIL`,
+
+		UPDATE_MODEL: `${moduleName}.UPDATE_MODEL`,
+		UPDATE_MODEL_SUCCESS: `${moduleName}.UPDATE_MODEL_SUCCESS`,
+		UPDATE_MODEL_FAIL: `${moduleName}.UPDATE_MODEL_FAIL`,
+
 		DELETE_MODEL: `${moduleName}.DELETE_MODEL`,
 		DELETE_MODEL_SUCCESS: `${moduleName}.DELETE_MODEL_SUCCESS`,
 		DELETE_MODEL_FAIL: `${moduleName}.DELETE_MODEL_FAIL`,
-
-		POST_MODEL: `${moduleName}.POST_MODEL`,
-		POST_MODEL_SUCCESS: `${moduleName}.POST_MODEL_SUCCESS`,
-		POST_MODEL_FAIL: `${moduleName}.POST_MODEL_FAIL`,
-
-		PUT_MODEL: `${moduleName}.PUT_MODEL`,
-		PUT_MODEL_SUCCESS: `${moduleName}.PUT_MODEL_SUCCESS`,
-		PUT_MODEL_FAIL: `${moduleName}.PUT_MODEL_FAIL`,
 	}
 
 	const actions = {
 		getModel: createAction(moduleActionTypes.GET_MODEL),
 		getModelSuccess: createAction(moduleActionTypes.GET_MODEL_SUCCESS),
 		getModelFail: createAction(moduleActionTypes.GET_MODEL_FAIL),
+
+		createModel: createAction(moduleActionTypes.CREATE_MODEL),
+		createModelSuccess: createAction(moduleActionTypes.CREATE_MODEL_SUCCESS),
+		createModelFail: createAction(moduleActionTypes.CREATE_MODEL_FAIL),
+
+		updateModel: createAction(moduleActionTypes.UPDATE_MODEL),
+		updateModelSuccess: createAction(moduleActionTypes.UPDATE_MODEL_SUCCESS),
+		updateModelFail: createAction(moduleActionTypes.UPDATE_MODEL_FAIL),
+
+		deleteModel: createAction(moduleActionTypes.DELETE_MODEL),
+		deleteModelSuccess: createAction(moduleActionTypes.DELETE_MODEL_SUCCESS),
+		deleteModelFail: createAction(moduleActionTypes.DELETE_MODEL_FAIL),
 	}
 
 	// ------------------------------------
@@ -35,7 +47,7 @@ const useModuleActions = (moduleName: string, path: string) => {
 	// ------------------------------------
 
 	const moduleActions = {
-		getModel: (params?: number, query?: string) => dispatch => {
+		getModel: (params?: number, query?: object) => dispatch => {
 			dispatch(actions.getModel())
 
 			api.requests.get(path, params, query).then(
@@ -44,6 +56,45 @@ const useModuleActions = (moduleName: string, path: string) => {
 				},
 				error => {
 					dispatch(actions.getModelFail(undefined, error))
+				},
+			)
+		},
+
+		createModel: (body, params?: number, query?: object) => dispatch => {
+			dispatch(actions.createModel())
+
+			api.requests.post(path, body, params, query).then(
+				data => {
+					dispatch(actions.createModelSuccess(data))
+				},
+				error => {
+					dispatch(actions.createModelFail(undefined, error))
+				},
+			)
+		},
+
+		updateModel: (body, params?: number, query?: object) => dispatch => {
+			dispatch(actions.updateModel())
+
+			api.requests.put(path, body, params, query).then(
+				data => {
+					dispatch(actions.updateModelSuccess(data))
+				},
+				error => {
+					dispatch(actions.updateModelFail(undefined, error))
+				},
+			)
+		},
+
+		deleteModel: (params?: number, query?: object) => dispatch => {
+			dispatch(actions.deleteModel())
+
+			api.requests.delete(path, params, query).then(
+				data => {
+					dispatch(actions.deleteModelSuccess(data))
+				},
+				error => {
+					dispatch(actions.deleteModelFail(undefined, error))
 				},
 			)
 		},
