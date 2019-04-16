169d161f5d16b7ed84e6b8885832978e01e10e44
HuyTrinh <huy@makehelsinki.com>
2019-04-16 18:41:56 +0300

    refactor: import order in index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,12 @@
 import * as React from 'react'
 import * as ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
+import {ConnectedRouter} from 'connected-react-router'
 import {configureStore, history} from './configStore'
 import App from './App'
-import './styles/index.scss'
 import './services/i18n'
-import {ConnectedRouter} from 'connected-react-router'
+import './services/api'
+import './styles/index.scss'
 
 const store = configureStore()
 
