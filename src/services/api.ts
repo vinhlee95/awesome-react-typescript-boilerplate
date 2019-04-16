6854051f7d294d6db6ae9f23bd24f4ee6316bf73
HuyTrinh <huy@makehelsinki.com>
2019-04-16 18:35:01 +0300

    fix: api service

--- src/services/api.ts
+++ src/services/api.ts
@@ -8,31 +8,33 @@
 import axios from 'axios'
 import {stringify} from 'query-string'
 
-let baseUrl: string = process.env.API_ENDPOINT
-	? process.env.API_ENDPOINT
-	: 'https://jsonplaceholder.typicode.com'
+const baseUrl: string =
+	process.env.API_ENDPOINT || 'https://jsonplaceholder.typicode.com'
 
 export const api: any = axios.create({baseURL: baseUrl})
 
 const getData = res => res.data
 const joinUrl = (path, params?, query?): string => {
-	let joinedUrl = `${path}`
+	let joinedUrl = path
+
 	if (params) {
 		joinedUrl = joinedUrl + '/' + params
 	}
+
 	if (query) {
-		joinedUrl = joinedUrl + '/' + `?${stringify(query)}`
+		joinedUrl = joinedUrl + `?${stringify(query)}`
 	}
+
 	return joinedUrl
 }
 
 export const requests = {
 	get: (path: string, params?, query?) =>
 		api.get(joinUrl(path, params, query)).then(getData),
-	post: (path: string, params?, query?) =>
-		api.post(joinUrl(path, params, query)).then(getData),
-	put: (path: string, params?, query?) =>
-		api.put(joinUrl(path, params, query)).then(getData),
+	post: (path: string, body, params?, query?) =>
+		api.post(joinUrl(path, params, query), body).then(getData),
+	put: (path: string, body, params?, query?) =>
+		api.put(joinUrl(path, params, query), body).then(getData),
 	delete: (path: string, params?, query?) =>
 		api.delete(joinUrl(path, params, query)).then(getData),
 }
