c1891fa5d2973b879b241ee14fb70ed63ae91d66
HuyTrinh <dinhhuyams@gmail.com>
2019-04-21 16:59:20 +0300

    feature: config react testing lib

--- jest.config.js
+++ jest.config.js
@@ -7,6 +7,7 @@
 
 module.exports = {
 	preset: 'ts-jest',
+	setupFilesAfterEnv: ['react-testing-library/cleanup-after-each'],
 	collectCoverageFrom: [
 		'src/**/*.{js,jsx,ts,tsx}',
 		'!src/**/*.test.{js,jsx,ts,tsx}',
