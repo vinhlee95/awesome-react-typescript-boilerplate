9e5601e52df1b649a29a033243de98517d138cc5
HuyTrinh <dinhhuyams@gmail.com>
2019-04-22 16:32:49 +0300

    test: render Post

--- src/pages/Home/component/Post/tests/Post.test.tsx
+++ src/pages/Home/component/Post/tests/Post.test.tsx
@@ -1,14 +1,26 @@
 import * as React from 'react'
-import {render} from 'react-testing-library'
+import {render, fireEvent} from 'react-testing-library'
+import 'jest-dom/extend-expect'
 import Post from '../Post'
+import PostModel from '../../../../../models/Post'
 import {postBuilder} from '../../../../../utils/testUtils'
+
+const renderPost = () => {
+	const mockPost: PostModel = postBuilder()
+	const mockOnClick = jest.fn()
+
+	const utils = render(<Post post={mockPost} onClick={mockOnClick} />)
+
+	return {...utils, mockPost, mockOnClick}
+}
 
 describe('<Post/>', () => {
 	it('should render Post', () => {
+		// Act
+		const {getByText, mockPost, debug} = renderPost()
 
-		// Arrange
-		const post = postBuilder()
-
-		const {getByText} = 
+		// Assert
+		expect(getByText(`id: ${mockPost.id}`)).toBeInTheDocument()
+		expect(getByText(`title: ${mockPost.title}`)).toBeInTheDocument()
 	})
 })
