69c659d0ad4b26d60204e65421788fa4a4aab91b
HuyTrinh <dinhhuyams@gmail.com>
2019-04-22 11:50:09 +0300

    chore: update jest config

--- jest.config.js
+++ jest.config.js
@@ -20,6 +20,10 @@ module.exports = {
 	clearMocks: true,
 	moduleDirectories: ['node_modules', 'src'],
 	moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'node', 'json'],
+	moduleNameMapper: {
+		"\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/testing/fileMock.ts",
+		"\\.(css|less|sass|scss)$": "<rootDir>/testing/styleMock.ts"
+	},
 	// configure minimum threshold enforcement for coverage results
 	// https://jestjs.io/docs/en/configuration#coveragethreshold-object
 	coverageThreshold: {
