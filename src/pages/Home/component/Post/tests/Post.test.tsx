4b224bb2fca416821b36748263b57f56e6227c18
HuyTrinh <dinhhuyams@gmail.com>
2019-04-22 16:29:34 +0300

    test: render Post

---
+++ src/pages/Home/component/Post/tests/Post.test.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import {render} from 'react-testing-library'
+import Post from '../Post'
+import {postBuilder} from '../../../../../utils/testUtils'
+
+describe('<Post/>', () => {
+	it('should render Post', () => {
+
+		// Arrange
+		const post = postBuilder()
+
+		const {getByText} = 
+	})
+})
