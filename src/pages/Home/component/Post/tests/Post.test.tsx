e58a6a91d33fa3e27be83f7b261fd43c1ef66b7b
HuyTrinh <huy@makehelsinki.com>
2019-04-23 11:09:49 +0300

    test: click on Post

--- src/pages/Home/component/Post/tests/Post.test.tsx
+++ src/pages/Home/component/Post/tests/Post.test.tsx
@@ -1,17 +1,18 @@
 import * as React from 'react'
-import {render} from 'react-testing-library'
+import {fireEvent, render, wait} from 'react-testing-library'
 import 'jest-dom/extend-expect'
 import Post from '../Post'
 import PostModel from '../../../../../models/Post'
 import {postBuilder} from '../../../../../utils/testUtils'
 
 const renderPost = () => {
+	// Arrange
 	const mockPost: PostModel = postBuilder()
 	const mockOnClick = jest.fn()
 
 	const utils = render(<Post post={mockPost} onClick={mockOnClick} />)
 
-	return {...utils, mockPost}
+	return {...utils, mockPost, mockOnClick}
 }
 
 describe('<Post/>', () => {
@@ -22,5 +23,16 @@ describe('<Post/>', () => {
 		// Assert
 		expect(getByText(`id: ${mockPost.id}`)).toBeInTheDocument()
 		expect(getByText(`title: ${mockPost.title}`)).toBeInTheDocument()
+	})
+
+	it('should be able to click on Post', () => {
+		// Act
+		const {getByTestId, mockPost, mockOnClick} = renderPost()
+
+		fireEvent.click(getByTestId('post-component'))
+
+		// Assert
+		expect(mockOnClick).toHaveBeenCalledTimes(1)
+		expect(mockOnClick).toHaveBeenCalledWith(mockPost.id)
 	})
 })
