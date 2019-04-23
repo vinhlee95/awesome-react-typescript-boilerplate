b6f7bd6b64a7498b211315fbca5aec382221b420
HuyTrinh <huy@makehelsinki.com>
2019-04-23 16:08:10 +0300

    test: load Posts and show initial Post Detail text

--- src/pages/Home/tests/Home.test.tsx
+++ src/pages/Home/tests/Home.test.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
 import 'jest-dom/extend-expect'
+import {fireEvent, getByText, wait} from 'react-testing-library'
 import {renderWithStore} from '../../../utils/testUtils'
+import {postBuilder} from '../../../utils/mockUtils'
+import {requests as mockRequests} from '../../../services/api'
 import Home from '../Home'
 
 describe('<Home/>', () => {
@@ -10,5 +13,38 @@ describe('<Home/>', () => {
 
 		// Assert
 		expect(getByText('common.welcome')).toBeInTheDocument()
+	})
+
+	it('should fetch Posts and render them to the page', async () => {
+		// Arrange
+		const post1 = postBuilder()
+		const post2 = postBuilder()
+		const post3 = postBuilder()
+
+		const mockPosts = [post1, post2, post3]
+
+		mockRequests.get = jest.fn(() => Promise.resolve(mockPosts))
+
+		// Act
+		const {getByText, queryByText} = renderWithStore(<Home />)
+
+		// Assert
+
+		expect(getByText('Loading ...')).toBeInTheDocument()
+
+		await wait(() => expect(queryByText('Loading ...')).toBeNull())
+
+		mockPosts.forEach(mockPost => {
+			expect(getByText(`id: ${mockPost.id}`)).toBeInTheDocument()
+			expect(getByText(`title: ${mockPost.title}`)).toBeInTheDocument()
+		})
+	})
+
+	it('should initially show Post Detail as default text', () => {
+		// Act
+
+		const {getByText} = renderWithStore(<Home />)
+
+		expect(getByText('Post Detail')).toBeInTheDocument()
 	})
 })
