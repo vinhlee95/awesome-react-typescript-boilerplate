6bdf0d9afa3df445b141680c156c9a5f4d56f65d
HuyTrinh <huy@makehelsinki.com>
2019-04-24 16:07:18 +0300

    refactor: Post component

--- src/pages/Home/component/Post/Post.tsx
+++ src/pages/Home/component/Post/Post.tsx
@@ -11,15 +11,17 @@ const Post = (props: Props) => {
 	const {post, onClick} = props
 
 	return (
-		<div
-			data-testid="post-component"
-			className="post"
-			key={post.id}
-			onClick={() => onClick(post.id)}
-		>
-			<p className="post__id">id: {post.id}</p>
-			<p className="post__title">title: {post.title}</p>
-		</div>
+		post && (
+			<div
+				data-testid="post-component"
+				className="post"
+				key={post.id}
+				onClick={() => onClick(post.id)}
+			>
+				<p className="post__id">id: {post.id}</p>
+				<p className="post__title">title: {post.title}</p>
+			</div>
+		)
 	)
 }
 
