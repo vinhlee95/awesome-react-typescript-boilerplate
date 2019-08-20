bcde2137dcf25a65ae0cf8498b2f08fe5624775a
HuyAms <dinhhuyams@gmail.com>
2019-08-20 22:01:02 +0300

    refactor: App.ts

--- src/modules/App.ts
+++ src/modules/App.ts
@@ -4,27 +4,6 @@ import {createAction} from './commons/common'
 import i18n from '../services/i18n'
 
 // ------------------------------------
-// Const
-// ------------------------------------
-const moduleName = 'app'
-
-// ------------------------------------
-// Action Creator
-// ------------------------------------
-
-const types = {
-	INITIALIZE: `@@${moduleName}/INITIALIZE`,
-	TEAR_DOWN: `@@${moduleName}/TEAR_DOWN`,
-	CHANGE_LANGUAGE: `@@${moduleName}/CHANGE_LANGUAGE`,
-}
-
-const actions = {
-	initialize: createAction(types.INITIALIZE),
-	tearDown: createAction(types.TEAR_DOWN),
-	changeLanguage: createAction(types.CHANGE_LANGUAGE),
-}
-
-// ------------------------------------
 // Reducer
 // ------------------------------------
 
@@ -40,6 +19,23 @@ export const appReducer = (state = initialState, action) =>
 				break
 		}
 	})
+
+// ------------------------------------
+// Const
+// ------------------------------------
+const moduleName = 'app'
+
+const types = {
+	INITIALIZE: `@@${moduleName}/INITIALIZE`,
+	TEAR_DOWN: `@@${moduleName}/TEAR_DOWN`,
+	CHANGE_LANGUAGE: `@@${moduleName}/CHANGE_LANGUAGE`,
+}
+
+const actions = {
+	initialize: createAction(types.INITIALIZE),
+	tearDown: createAction(types.TEAR_DOWN),
+	changeLanguage: createAction(types.CHANGE_LANGUAGE),
+}
 
 // ------------------------------------
 // Actions
